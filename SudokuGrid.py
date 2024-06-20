f75fac475c94b12b56a1d2ed0a01081c80fd506f
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-20 14:59:35 +0200

    working on selecting cell

---
+++ SudokuGrid.py
@@ -0,0 +1,31 @@
+import numpy as np
+class SudokuGrid:
+    def __init__(self):
+        # self.grid=np.zeros([9,9], dtype=int)
+        self.grid=([[1,0,7,0,0,6,4,5,0],
+                    [0,2,5,3,4,0,0,0,8],
+                    [0,6,0,0,0,1,0,7,0],
+                    [0,5,3,0,0,0,0,2,9],
+                    [6,1,0,0,0,9,8,0,0],
+                    [0,0,0,6,0,2,0,0,7],
+                    [0,0,1,0,9,3,2,0,0],
+                    [0,0,8,0,0,0,0,0,0],
+                    [0,4,0,0,7,8,5,9,1]])
+
+    def print_grid(self):
+        for i in range (0,9):
+            if ((i)%3==0 and i!=0):
+                print("------+-------+------")
+            for j in range (0,9):
+                print(self.grid[i][j], end=' ')
+                if ((j+1)%3==0 and j!=8):
+                    print("|", end=' ')
+            print()    
+        print() 
+    
+    def set_number(self, row, col, n):
+        self.grid[col][row]=n
+
+    def get_pos(self,row,col):
+        return(self.grid[row][col])
+        
--- gui.py
+++ gui.py
@@ -1,17 +1,8 @@
 import pygame
+import numpy as np
 import sys
 from settings import *
-
-grid=([[1,0,7,0,0,6,4,5,0],
-      [0,2,5,3,4,0,0,0,8],
-      [0,6,0,0,0,1,0,7,0],
-      [0,5,3,0,0,0,0,2,9],
-      [6,1,0,0,0,9,8,0,0],
-      [0,0,0,6,0,2,0,0,7],
-      [0,0,1,0,9,3,2,0,0],
-      [0,0,8,0,0,0,0,0,0],
-      [0,4,0,0,7,8,5,9,1]])
-
+from SudokuGrid import *
 
 def draw_background():
     screen.fill((200,200,200))
@@ -19,7 +10,6 @@ def draw_background():
     draw_number(w)
     # draw_buttons()
     return w
-
 
 def draw_grid():
     s=SCREEN_HEIGHT-20
@@ -41,43 +31,66 @@ def draw_buttons():
 def draw_number(s):
     for row in range (0,9):
         for col in range(0,9):
-            output=grid[row][col]
+            output=grid.get_pos(row,col)
             if(output!=0):
                 n_text=font.render(str(output), True, (0,0,0))
                 screen.blit(n_text, pygame.Vector2(row*(s//9)+25, col*(s//9)+21))
 
-def colora_cella(xy):
-    font = pygame.font.Font('freesansbold.ttf', 32)
-    prop=w/SCREEN_WIDTH
+def set_cella(xy):
     x=xy[0]
     y=xy[1]
-    if (x<=w and y<=w):
-        x=x//(w/9)
-        y=y//(w/9)
-        print(str(x)+" "+str(y))
-        pygame.draw.rect(screen, (255,0,0), pygame.Rect(x*(w//9), y*(w//9), (x+1)*(w//9), (y+1)*(w//9)))
-        print(str(x*(w//9))+" " + str(y*(w//9)) + " " + str((x+1)*(w//9)) + " " + str((y+1)*(w//9)))
+    # if (x<=w and y<=w):
+    #     x=x//(w/9)
+    #     y=y//(w/9)
+    #     print(str(x)+" "+str(y))
+    #     pygame.draw.rect(screen, (255,0,0), pygame.Rect(x*(w//9), y*(w//9), (x+1)*(w//9), (y+1)*(w//9)))
+    #     print(str(x*(w//9))+" " + str(y*(w//9)) + " " + str((x+1)*(w//9)) + " " + str((y+1)*(w//9)))
+    if (x<=grid_width and y<=grid_width):
+        x= int(x//(grid_width/9))
+        y = int(y//(grid_width/9))
+        offses1=0
+        offses2=0
+        if(x==0 or x==3 or x==6):
+            offses1=12
+            print("offset1")
+            print(offses1)
+
+        if(y==0 or y==3 or y==6):
+            offses2=12
+            print("offset2")
+            print(offses2)
+
+
+        pygame.draw.rect(screen, (100,100,200), pygame.Rect((grid_width/9)*x+offses1, (grid_width/9)*y+offses2, (grid_width/9)+(x+1), (grid_width/9)+(y+1) ))
+        # print(grid_width)
+        # print((grid_width/9)*x+12)
+        # print((grid_width/9)*y+12)
+        # print((grid_width/9)+(x+1))
+        # print((grid_width/9)+(y+1))
+        return( (x, y) )
+    else:
+        return((-1,-1))
+
 
 pygame.init()
 
 clock=pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), flags, vsync=1)
-pygame.font.init() # you have to call this at the start, 
-                   # if you want to use this module.
+pygame.font.init()
 font = pygame.font.SysFont('arial', 20)
-print(pygame.font.get_fonts())
 
+grid = SudokuGrid()
 
 
 run  = True
-w=draw_background()
+grid_width=draw_background()
 while run:
 
     for event in pygame.event.get():
         if (event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE)):
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            colora_cella(pygame.mouse.get_pos())
+            print(set_cella(pygame.mouse.get_pos()))
 
     pygame.display.flip()
     clock.tick(30)
