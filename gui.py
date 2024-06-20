a04830c24db755fa0a8d4e50969469d00e8fa3d0
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-20 09:14:32 +0200

    .

--- gui.py
+++ gui.py
@@ -4,20 +4,30 @@ from settings import *
 
 def draw_background():
     screen.fill((255,255,255))
-    draw_lines()
+    w=draw_grid()
+    return w
 
 
-def draw_lines():
-    w=SCREEN_WIDTH-5
-    h=SCREEN_HEIGHT-5
+def draw_grid():
+    w=SCREEN_WIDTH/2
+    h=SCREEN_HEIGHT/2
     for i in range (0,10):
         if (i%3==0 or i==0 or i==9):
             width=5
         else:
             width=2
-        pygame.draw.line(screen, (0,0,0),(w/9*i+2,0),(w/9*i+2,h), width)
-        pygame.draw.line(screen, (0,0,0),(0,h/9*i+2),(w,h/9*i+2), width)
+        pygame.draw.line(screen, (0,0,0),(w/9*i+2,0),(w/9*i+2,h+4), width)
+        pygame.draw.line(screen, (0,0,0),(0,h/9*i+2),(w+4,h/9*i+2), width)
+    return(w)
 
+def colora_cella(xy):
+    x=xy[0]
+    y=xy[1]
+    if (x<=w and y<=w):
+        print(x+" "+y)
+        x=x//(w/9)
+        y=y//(w/9)
+        pygame.draw.rect(screen, (255,0,0), pygame.Rect(x, y, x+(w/9), y+(w/9)))
 
 pygame.init()
 
@@ -33,7 +43,9 @@ while run:
     for event in pygame.event.get():
         if (event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE)):
             run = False
-    draw_background()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            colora_cella(pygame.mouse.get_pos())
+    w=draw_background()
     pygame.display.flip()
     clock.tick(30)
     
