76697bc115a4b457c9d631bd8b8232bacae72258
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-20 16:14:30 +0200

    select cell done

--- gui.py
+++ gui.py
@@ -6,62 +6,87 @@ from SudokuGrid import *
 
 def draw_background():
     screen.fill((200,200,200))
-    w=draw_grid()
-    draw_number(w)
-    # draw_buttons()
-    return w
+    grid_width=draw_grid()
+    draw_numbers(grid_width)
+    return grid_width
 
 def draw_grid():
     s=SCREEN_HEIGHT-20
     for i in range (0,10):
-        if (i==0 or i==9):
-            width=5
-        elif(i%3==0):
-            width=4
+        if (i%3==0 or i==0 or i==9):
+            width=3
         else:
-            width=2
-        pygame.draw.line(screen, (0 ,0,0),(s/9*i+9,9),(s/9*i+9,s+11), width)
-        pygame.draw.line(screen, (0,0,0),(9,s/9*i+9),(s+11,s/9*i+9), width)
+            width=1
+        pygame.draw.line(screen, (0 ,0,0),(s/9*i+9,9),(s/9*i+9,s+9), width)
+        pygame.draw.line(screen, (0,0,0),(9,s/9*i+9),(s+9,s/9*i+9), width)
     return(s)
 
 def draw_buttons():
     text_surface = font.render('Some Text', False, (0, 0, 0), (255,255,255))
     screen.blit(text_surface, (420,20))
 
-def draw_number(s):
+def draw_numbers(grid_width):
     for row in range (0,9):
         for col in range(0,9):
             output=grid.get_pos(row,col)
             if(output!=0):
                 n_text=font.render(str(output), True, (0,0,0))
-                screen.blit(n_text, pygame.Vector2(row*(s//9)+25, col*(s//9)+21))
+                screen.blit(n_text, pygame.Vector2(row*(grid_width//9)+25, col*(grid_width//9)+21))
 
-def set_cella(xy):
+def draw_number(grid_widths, row, col):
+    output=grid.get_pos(row,col)
+    if(output!=0):
+        n_text=font.render(str(output), True, (0,0,0))
+        screen.blit(n_text, pygame.Vector2(row*(grid_width//9)+25, col*(grid_width//9)+21))
+
+def set_cella(xy, prev_xy):
+    _=draw_background()
     x=xy[0]
     y=xy[1]
+    x= int(x//(grid_width/9))
+    y = int(y//(grid_width/9))
+    print("xy ")
+    print(xy)
+    print("prev")
+    print(prev_xy)
     # if (x<=w and y<=w):
     #     x=x//(w/9)
     #     y=y//(w/9)
     #     print(str(x)+" "+str(y))
     #     pygame.draw.rect(screen, (255,0,0), pygame.Rect(x*(w//9), y*(w//9), (x+1)*(w//9), (y+1)*(w//9)))
     #     print(str(x*(w//9))+" " + str(y*(w//9)) + " " + str((x+1)*(w//9)) + " " + str((y+1)*(w//9)))
+    if(prev_xy[0]==x and prev_xy[1]==y):
+        print("reset")
+        _=draw_background()
+        return((-1,-1))
+    
+
     if (x<=grid_width and y<=grid_width):
-        x= int(x//(grid_width/9))
-        y = int(y//(grid_width/9))
-        offses1=0
-        offses2=0
-        if(x==0 or x==3 or x==6):
-            offses1=12
-            print("offset1")
-            print(offses1)
+        offset1=10
+        offset2=10
+        offset3=x+5-(x%3)
+        offset4=y+5-(y%3)
+        if(x%3==0):
+            offset1+=1
+            offset3-=2
+        if(y%3==0):
+            offset2+=1
+            offset4-=2
 
-        if(y==0 or y==3 or y==6):
-            offses2=12
-            print("offset2")
-            print(offses2)
+        if((x+1)%3==0):
+            offset3+=0
+        if((y+1)%3==0):
+            offset4+=0
+        
+        # print("offset1 "+str(offset1))
+        # print("offset2 "+str(offset2))
+        # print("offset3 "+str(offset3))
+        # print("offset4 "+str(offset4))
 
 
-        pygame.draw.rect(screen, (100,100,200), pygame.Rect((grid_width/9)*x+offses1, (grid_width/9)*y+offses2, (grid_width/9)+(x+1), (grid_width/9)+(y+1) ))
+
+        pygame.draw.rect(screen, (100,100,200), pygame.Rect((grid_width/9)*x+offset1, (grid_width/9)*y+offset2, (grid_width/9)+(x+1)-offset3, (grid_width/9)+(y+1)-offset4 ))
+        draw_number(grid_width,x,y)
         # print(grid_width)
         # print((grid_width/9)*x+12)
         # print((grid_width/9)*y+12)
@@ -84,13 +109,26 @@ grid = SudokuGrid()
 
 run  = True
 grid_width=draw_background()
+selected_cell=(-1,-1)
 while run:
 
     for event in pygame.event.get():
         if (event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE)):
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(set_cella(pygame.mouse.get_pos()))
+            selected_cell=set_cella(pygame.mouse.get_pos(),selected_cell)
+        if (event.type == pygame.KEYDOWN and (event.key==pygame.K_0 or 
+                                              event.key==pygame.K_1 or 
+                                              event.key==pygame.K_2 or 
+                                              event.key==pygame.K_3 or
+                                              event.key==pygame.K_4 or
+                                              event.key==pygame.K_5 or
+                                              event.key==pygame.K_6 or
+                                              event.key==pygame.K_7 or
+                                              event.key==pygame.K_8 or
+                                              event.key==pygame.K_9 or
+
+                                              )):
 
     pygame.display.flip()
     clock.tick(30)
