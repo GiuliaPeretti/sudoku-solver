0faf9ea249491bc92531235cf8e81a5e3ab9c360
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-21 20:06:16 +0200

    difficulty selection
    added button function, difficulty selection

--- gui.py
+++ gui.py
@@ -5,8 +5,9 @@ import random
 import copy
 
 def draw_background():
-    screen.fill((200,200,200))
+    screen.fill(BACKGROUND_COLOR)
     grid_width=draw_grid()
+    draw_button()
     draw_numbers(grid_width)
     return grid_width
 
@@ -22,22 +23,23 @@ def draw_grid():
     return(s)
 
 def draw_numbers(grid_width):
-    for row in range (0,9):
-        for col in range(0,9):
-            output=grid[row][col]
-            if(output!=0):
-                if (grid_black[row][col]==1):
-                    color=(0,0,0)
-                elif (grid_solved[row][col]!=grid[row][col]):
-                    color=(255,0,0)
-                else:
-                    color=(0,0,255)
-                n_text=font.render(str(output), True, color)
+    if(grid is not None):
+        for row in range (0,9):
+            for col in range(0,9):
+                output=grid[row][col]
+                if(output!=0):
+                    if (grid_bold[row][col]==1):
+                        color=(0,0,0)
+                    elif (grid_solved[row][col]!=grid[row][col]):
+                        color=(255,0,0)
+                    else:
+                        color=(0,0,255)
+                    n_text=font.render(str(output), True, color)
 
-                screen.blit(n_text, (col*(grid_width//9)+24, row*(grid_width//9)+18))
+                    screen.blit(n_text, (col*(grid_width//9)+24, row*(grid_width//9)+18))
 
 def draw_number(row, col, n):
-    if (grid_black[row][col]==1):
+    if (grid_bold[row][col]==1):
         color=(0,0,0)
     elif (grid_solved[row][col]!=n):
         color=(255,0,0)
@@ -48,13 +50,13 @@ def draw_number(row, col, n):
         screen.blit(n_text, (col*(grid_width//9)+24, row*(grid_width//9)+18))
 
 def draw_button():
-    x1,y1,w,h=SCREEN_WIDTH-50-110, 9, 95, 30
+    x1,y1,w,h=SCREEN_WIDTH-50-110, 9, 120, 30
     pygame.draw.rect(screen, (200,200,200), (x1,y1,w,h))
     pygame.draw.rect(screen, (0,0,0), (x1,y1,w,h), 2)
 
     font = pygame.font.SysFont('arial', 17)
     text=font.render(str("Easy"), True, (0,0,0))
-    screen.blit(text, (x1+27, y1+5))
+    screen.blit(text, (x1+40, y1+5))
 
     b_easy=((x1,x1+w),(y1,y1+h))
 
@@ -63,7 +65,7 @@ def draw_button():
     pygame.draw.rect(screen, (0,0,0), (x1,y1,w,h), 2)
 
     text=font.render(str("Medium"), True, (0,0,0))
-    screen.blit(text, (x1+20, y1+5))
+    screen.blit(text, (x1+30, y1+5))
 
     b_medium=((x1,x1+w),(y1,y1+h))
 
@@ -72,7 +74,7 @@ def draw_button():
     pygame.draw.rect(screen, (0,0,0), (x1,y1,w,h), 2)
 
     text=font.render(str("Difficult"), True, (0,0,0))
-    screen.blit(text, (x1+20, y1+5))
+    screen.blit(text, (x1+30, y1+5))
 
     b_difficult=((x1,x1+w),(y1,y1+h))
 
@@ -81,53 +83,58 @@ def draw_button():
     pygame.draw.rect(screen, (0,0,0), (x1,y1,w,h), 2)
 
     text=font.render(str("Generate new"), True, (0,0,0))
-    screen.blit(text, (x1+9, y1+5))
+    screen.blit(text, (x1+8, y1+5))
 
     b_generate=((x1,x1+w),(y1,y1+h))
 
     return(b_easy,b_medium,b_difficult,b_generate)
 
 def set_cella(xy, prev_xy):
-    _=draw_background()
-    col=xy[0]
-    row=xy[1]
-
-    if(col>grid_width or row>grid_width):
-        return((-1,-1))
-
-    col= int(col//(grid_width/9))
-    row = int(row//(grid_width/9))
-
-    if(prev_xy[0]==col and prev_xy[1]==row):
+    if(grid is not None):
         _=draw_background()
-        return((-1,-1))
-    
-    if (col<=grid_width and row<=grid_width):
-        offset1=10
-        offset2=10
-        offset3=col+5-(col%3)
-        offset4=row+5-(row%3)
-        if(col%3==0):
-            offset1+=1
-            offset3-=2
-        if(row%3==0):
-            offset2+=1
-            offset4-=2
-        if((col-1)%3==0):
-            offset3-=3
-        if((row-1)%3==0):
-            offset4-=3
-        pygame.draw.rect(screen, (100,200,100), pygame.Rect((grid_width/9)*col+offset1, (grid_width/9)*row+offset2, (grid_width/9)+(col+1)-offset3, (grid_width/9)+(row+1)-offset4 ))
-        draw_number(row, col, grid[row][col])
-        return((row, col))
-    else:
-        return((-1,-1))
+        col=xy[0]
+        row=xy[1]
 
-def init_grid():    
-    grid_solved=generate_grid()
-    grid=playble_grid(grid_solved,10)
-    grid_bold=generate_bold(grid)
-    return(grid, grid_bold, grid_solved)
+        if(col>grid_width or row>grid_width):
+            return((-1,-1))
+
+        col= int(col//(grid_width/9))
+        row = int(row//(grid_width/9))
+
+        if(prev_xy[0]==col and prev_xy[1]==row):
+            _=draw_background()
+            return(-1,-1)
+        
+        if (col<=grid_width and row<=grid_width):
+            offset1=10
+            offset2=10
+            offset3=col+5-(col%3)
+            offset4=row+5-(row%3)
+            if(col%3==0):
+                offset1+=1
+                offset3-=2
+            if(row%3==0):
+                offset2+=1
+                offset4-=2
+            if((col-1)%3==0):
+                offset3-=3
+            if((row-1)%3==0):
+                offset4-=3
+            pygame.draw.rect(screen, (100,200,100), pygame.Rect((grid_width/9)*col+offset1, (grid_width/9)*row+offset2, (grid_width/9)+(col+1)-offset3, (grid_width/9)+(row+1)-offset4 ))
+            draw_number(row, col, grid[row][col])
+            return(row, col)
+        else:
+            return(-1,-1)
+    else:
+        return(-1,-1)
+
+def init_grid(difficulty):    
+    if(difficulty!=0):
+        grid_solved=generate_grid()
+        grid=playble_grid(grid_solved,difficulty)
+        grid_bold=generate_bold(grid)
+        return(grid, grid_bold, grid_solved)
+    return(None,None,None)
 
 def generate_bold(grid):
     grid_bold=copy.deepcopy(grid)
@@ -179,6 +186,26 @@ def print_grid(grid):
         print()    
     print() 
 
+
+def select_difficulty(xy,s):
+    draw_button()
+    if(xy is not None):
+        print(xy)
+        x1,y1,w,h=xy[0][0], xy[1][0], 120, 30
+        pygame.draw.rect(screen, (250,100,150), (x1,y1,w,h))
+        pygame.draw.rect(screen, (0,0,0), (x1,y1,w,h), 2)
+
+        match (s):
+            case "Easy":
+                offset=40
+            case "Medium":
+                offset=30
+            case "Difficult":
+                offset=30
+        
+        font = pygame.font.SysFont('arial', 17)
+        text=font.render(s, True, (0,0,0))
+        screen.blit(text, (x1+offset, y1+5))
             
 
 
@@ -190,36 +217,53 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), flags, vsync=1)
 pygame.display.set_caption('Sudoku♥')
 font = pygame.font.SysFont('arial', 25)
 
-grid, grid_black, grid_solved = init_grid()
-print("grid")
-print_grid(grid)
-print("grid_black")
-print_grid(grid_black)
-print("grid_solved")
-print_grid(grid_solved)
-
-print_grid(grid_solved)
-
 
 run  = True
-grid_width=draw_background()
 selected_cell=(-1,-1)
 b_easy,b_medium,b_difficult,b_generate=draw_button()
-while run:
+grid=None
+difficulty=0
+grid_width=draw_background()
 
+while run:
     for event in pygame.event.get():
         if (event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_ESCAPE)):
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            selected_cell=set_cella(pygame.mouse.get_pos(),selected_cell)
-            print("Cella selezionate:")
-            print(selected_cell)
+            x,y=pygame.mouse.get_pos()
+            if(x<grid_width and y<grid_width):
+                print(selected_cell)
+                selected_cell=set_cella(pygame.mouse.get_pos(),selected_cell)
+                print("Cella selezionate:")
+                print(selected_cell)
+            elif(x>b_easy[0][0] and x<=b_easy[0][1] and y>=b_easy[1][0] and y<=b_easy[1][1]):
+                print("easy")
+                select_difficulty(b_easy,"Easy")
+                difficulty=20
+            elif(x>b_medium[0][0] and x<=b_medium[0][1] and y>=b_medium[1][0] and y<=b_medium[1][1]):
+                print("medium")
+                select_difficulty(b_medium,"Medium")
+                difficulty=40
+            elif(x>b_difficult[0][0] and x<=b_difficult[0][1] and y>=b_difficult[1][0] and y<=b_difficult[1][1]):
+                print("difficult")
+                select_difficulty(b_difficult,"Difficult")
+                difficulty=60
+            elif(x>b_generate[0][0] and x<=b_generate[0][1] and y>=b_generate[1][0] and y<=b_generate[1][1]):
+                print("generate_new")
+                grid, grid_bold, grid_solved = init_grid(difficulty)
+                difficulty=0
+                draw_background()
+                draw_numbers(grid_width)
+            else:
+                select_difficulty(None,"Difficult")
+                difficulty=0
         if (event.type == pygame.KEYDOWN):
             for i in range(len(INPUTS)):
                 if (event.key==INPUTS[i]):
                     if(selected_cell[0]!=-1 and grid[selected_cell[0]][selected_cell[1]])==0:
                         draw_number(selected_cell[0],selected_cell[1], i+1)
                         grid[selected_cell[0]][selected_cell[1]]=i+1
+                    
 
     pygame.display.flip()
     clock.tick(30)
--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ SCREEN_WIDTH=600
 SCREEN_HEIGHT=400
 flags=(pygame.HWSURFACE | pygame.DOUBLEBUF)
 INPUTS=[pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9]
+BACKGROUND_COLOR=(240,240,240)
